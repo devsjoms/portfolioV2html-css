e9953debc446118a4498c701f0deba3e615c69b9
jomsGutHib <jomarnaval023@gmail.com>
2025-06-17 12:11:56 +0800

    updated

---
+++ script.js
@@ -0,0 +1,9 @@
+function showCerts() {
+      document.getElementById('certBoxOverlay').style.display = 'block';
+      document.getElementById('certs-box').style.display = 'block';
+    }
+
+    function hideCerts() {
+      document.getElementById('certBoxOverlay').style.display = 'none';
+      document.getElementById('certs-box').style.display = 'none';
+    }
